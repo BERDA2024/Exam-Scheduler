62db9b649c79fb77216166173c3c295ec00800b7
B3N14M1N <beniamin.cioban@student.usv.ro>
2024-11-17 18:07:18 +0200

    [BERDA] Controllers for CRUP operations for all models

--- ExamScheduler.Server/Source/DataBase/ApplicationDbContext.cs
+++ ExamScheduler.Server/Source/DataBase/ApplicationDbContext.cs
@@ -5,9 +5,21 @@ namespace ExamScheduler.Server.Source.DataBase
 {
     public class ApplicationDbContext : DbContext
     {
-        public DbSet<ScheduleRequest> ScheduleRequest { get; set; }
-        public DbSet<Classroom> Classroom { get; set; }
-        public DbSet<Professor> Professor { get; set; }
+        public DbSet<ScheduleRequest> ScheduleRequest { get; set; } = default!;
+        public DbSet<Classroom> Classroom { get; set; } = default!;
+        public DbSet<Professor> Professor { get; set; } = default!;
+        public DbSet<Address> Address { get; set; } = default!;
+        public DbSet<Availability> Availability { get; set; } = default!;
+        public DbSet<Country> Country { get; set; } = default!;
+        public DbSet<Group> Group { get; set; } = default!;
+        public DbSet<Institute> Institute { get; set; } = default!;
+        public DbSet<RequestState> RequestState { get; set; } = default!;
+        public DbSet<Role> Role { get; set; } = default!;
+        public DbSet<Secretary> Secretary { get; set; } = default!;
+        public DbSet<Specialization> Specialization { get; set; } = default!;
+        public DbSet<Student> Student { get; set; } = default!;
+        public DbSet<Subject> Subject { get; set; } = default!;
+        public DbSet<User> User { get; set; } = default!;
 
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
@@ -17,6 +29,5 @@ namespace ExamScheduler.Server.Source.DataBase
         {
             base.OnModelCreating(modelBuilder);
         }
-        public DbSet<ExamScheduler.Server.Source.Models.Address> Address { get; set; } = default!;
     }
 }
