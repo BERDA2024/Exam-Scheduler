2106be4568a581e82430f7e571bdb0d1bdb07416
NistorElena-Simona <elena.nistor1@student.usv.ro>
2024-11-18 13:49:35 +0200

    login_page_start

---
+++ examscheduler.client/src/Login.jsx
@@ -0,0 +1,58 @@
+﻿import React from "react";
+import "./Login.css";
+import sample_img from "./assets/sample_img.jpeg"; // Importă imaginea folosind calea relativă
+
+const Login = () => {
+    return (
+        <div className="container">
+
+            {/* Secțiunea stângă */}
+            <div className="left-section">         
+
+                <form> 
+
+                    <div>
+
+                        <h1>Welcome to ExamScheduler!</h1>
+
+                    </div>
+                
+                  
+                    <div>
+                        <input type="email" placeholder="Email" />
+                        <input type="password" placeholder="Password" />
+
+                    </div>
+                    
+                    <button 
+                        type="button"
+                        className="login-btn"
+                        onClick={() => alert("Login clicked!")}
+                    >
+                        Login
+                    </button>
+                    <button
+                        type="button"
+                        className="signup-btn"
+                        onClick={() => alert("Sign Up clicked!")}
+                    >
+                        Sign Up
+                    </button>
+                  
+                </form>
+            </div>
+
+            {/* Secțiunea dreaptă */}
+            <div
+                className="right-section"
+                style={{
+                    backgroundImage: `url(${sample_img})`, // Folosește imaginea ca fundal
+                    backgroundSize: "cover",
+                    backgroundPosition: "center",
+                }}
+            ></div>
+        </div>
+    );
+};
+
+export default Login;
