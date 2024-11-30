2735629cbb587101d8035ca570b4e199aa2aef53
B3N14M1N <beniamin.cioban@student.usv.ro>
2024-11-30 16:35:30 +0200

    [BERDA] Changed Dashboard page

--- examscheduler.client/src/Pages/DashboardPage.jsx
+++ examscheduler.client/src/Pages/DashboardPage.jsx
@@ -14,51 +14,39 @@ const DashboardPage = () => {
                 <div className="main-content">
                     <ScrollableContainer>
                         <StylizedBlock title="Availability">
-                            {/* Recents block */}
-                            <div className="block recents">
-                                <h3>Recents</h3>
-                                <div className="block-item">
-                                    <AvailabilityForm />
-                                </div>
+                            {/* Availability block */}
+                            <div className="block-item">
+                                <AvailabilityForm />
                             </div>
                         </StylizedBlock>
 
-                        <StylizedBlock title="Availability">
+                        <StylizedBlock title="Agenda">
                             {/* Agenda block */}
-                            <div className="block agenda">
-                                <h3>Agenda</h3>
-                                <div className="block-item">
-                                    <span>16:00 | Examen Ingineria Programelor | Turcu Cristina Elena</span>
-                                </div>
+                            <div className="block-item">
+                                <span>16:00 | Examen Ingineria Programelor | Turcu Cristina Elena</span>
                             </div>
                         </StylizedBlock>
 
-                        <StylizedBlock title="Availability">
+                        <StylizedBlock title="Docs">
                             {/* Docs block */}
-                            <div className="block docs">
-                                <h3>Docs</h3>
-                                <div className="block-item">
-                                    <span>12/10/24 15:00 | Organizare examen Ingineria Programelor</span>
-                                </div>
+                            <div className="block-item">
+                                <span>12/10/24 15:00 | Organizare examen Ingineria Programelor</span>
                             </div>
                         </StylizedBlock>
 
-                        <StylizedBlock title="Availability">
+                        <StylizedBlock title="Inbox">
                             {/* Inbox block */}
-                            <div className="block inbox">
-                                <h3>Inbox</h3>
-                                <div className="block-item">
-                                    <span>You have an exam today at 16:00 in C202</span>
-                                </div>
-                                <div className="block-item">
-                                    <span>You have an exam tomorrow, 22/10/24, at 16:00</span>
-                                </div>
-                                <div className="block-item">
-                                    <span>New Doc was added by Turcu Cristina Elena at 15:00 12/10/24</span>
-                                </div>
-                                <div className="block-item">
-                                    <span>New Exam was added at 14:46 12/10/24</span>
-                                </div>
+                            <div className="block-item">
+                                <span>You have an exam today at 16:00 in C202</span>
+                            </div>
+                            <div className="block-item">
+                                <span>You have an exam tomorrow, 22/10/24, at 16:00</span>
+                            </div>
+                            <div className="block-item">
+                                <span>New Doc was added by Turcu Cristina Elena at 15:00 12/10/24</span>
+                            </div>
+                            <div className="block-item">
+                                <span>New Exam was added at 14:46 12/10/24</span>
                             </div>
                         </StylizedBlock>
                     </ScrollableContainer>
