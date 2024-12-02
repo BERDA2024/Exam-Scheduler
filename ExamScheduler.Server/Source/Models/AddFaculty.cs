04f26cb7578fd1956e6f3a25ba9448d3b6c12b08
NistorElena-Simona <elena.nistor1@student.usv.ro>
2024-12-02 16:06:01 +0200

    tryal_cypress

---
+++ ExamScheduler.Server/Source/Models/AddFaculty.cs
@@ -0,0 +1,10 @@
+﻿namespace ExamScheduler.Server.Source.Models
+{
+    public class AddFaculty
+    {
+        public int Id { get; set; }
+        public required string ShortName { get; set; }
+
+        public required string LongName { get; set; }
+    }
+}
