6b268ed8333d121cb48bd3c98ec24d8f8ac13296
NistorElena-Simona <elena.nistor1@student.usv.ro>
2024-12-10 15:03:31 +0200

    start

---
+++ ExamScheduler.Server/Controllers/DepartmentController.cs
@@ -0,0 +1,117 @@
+﻿using ExamScheduler.Server.Source.DataBase;
+using ExamScheduler.Server.Source.Domain;
+using ExamScheduler.Server.Source.Models;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+using System.Security.Claims;
+
+namespace ExamScheduler.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DepartmentController(ApplicationDbContext context, UserManager<User> userManager) : ControllerBase
+    {
+        private readonly UserManager<User> _userManager = userManager;
+        private readonly ApplicationDbContext _context = context;
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllDepartments()
+        {
+            var departments = await _context.Department
+                .ToListAsync();
+
+            return Ok(departments);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetDepartmentById(int id)
+        {
+            var dep = await _context.Department
+                .FirstOrDefaultAsync(a => a.Id == id);
+
+            if (dep == null)
+            {
+                return NotFound(new { message = "Department not found" });
+            }
+
+            return Ok(dep);
+        }
+
+        [Authorize(Roles = "Admin,FacultyAdmin")]
+        [HttpPost]
+        public async Task<IActionResult> CreateDepartment([FromBody] DepartmentModel request)
+        {
+            try
+            {
+                var userId = User.FindFirstValue(ClaimTypes.NameIdentifier); // Get user ID from the JWT
+
+                if (userId == null || request == null)
+                {
+                    return Unauthorized(new { message = "Not connected or bad request." });
+                }
+
+               // _context.Department.Add(request);
+                await _context.SaveChangesAsync();
+
+                return CreatedAtAction(nameof(GetDepartmentById), new { id = request.Id }, request);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = "SomeError: " + ex.Message });
+            }
+        }
+
+        [Authorize(Roles = "Admin,FacultyAdmin")]
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateDepartment(int id, [FromBody] DepartmentModel request)
+        {
+            if (id != request.Id)
+            {
+                return BadRequest(new { message = "ID mismatch" });
+            }
+
+            _context.Entry(request).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!DepartmentExists(id))
+                {
+                    return NotFound(new { message = "Faculty not found" });
+                }
+                throw;
+            }
+
+            return NoContent();
+        }
+
+        [Authorize(Roles = "Admin,FacultyAdmin")]
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteDepartment(int id)
+        {
+            var depa= await _context.Department.FindAsync(id);
+            if (depa == null)
+            {
+                return NotFound(new { message = "Department not found" });
+            }
+
+            _context.Department.Remove(depa);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+       
+        private bool DepartmentExists(int id)
+        {
+            return _context.Department.Any(a => a.Id == id);
+        }
+    }
+}
---
+++ ExamScheduler.Server/Source/Models/DepartmentModel.cs
@@ -0,0 +1,12 @@
+﻿namespace ExamScheduler.Server.Source.Models
+{
+    public class DepartmentModel
+
+    {
+
+        public  int Id { get; set; }
+        public string Faculty_Name { get; set; }
+        public string Short_Name { get; set; }
+        public string Long_Name { get; set; }
+    }
+}
