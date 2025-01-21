bd5e26565ab5097792f97bdc4e294fb3190fd9d2
rZn24h <razvan.hertanu@student.usv.ro>
2025-01-21 19:35:59 +0200

    ExamsTable
    Lista cu toate examenele pentru studenti.

---
+++ examscheduler.client/src/Pages/ExamsPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ExamsTable from "../Components/ExamTable";
+
+const ExamsPage = () => {
+    return (
+        <div>
+            <h1>Exams</h1>
+            <ExamsTable />
+        </div>
+    );
+};
+
+export default ExamsPage;
